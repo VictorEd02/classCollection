6d2f117250e8aeb7c37cc2d1df24f12e919ebd1f
VictorEd02 <victor.edgard.neves@gmail.com>
2021-07-01 16:44:46 -0300

    created test of operations

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios){
+        println(salario)
+    }
+
+    println("-----------------------------------")
+    println("Maior salarios: ${salarios.maxOrNull()}")
+    println("Menor salarios: ${salarios.minOrNull()}")
+    println("Media salarios: ${salarios.average()}")
+
+    val salariosMaiorque2500 = salarios.filter { it > 2500.0 }
+    println("-----------------------------------")
+    salariosMaiorque2500.forEach { println(it) }
+}
